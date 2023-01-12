5570744a0583d141ced253f21883a37ceb2b693b
rmnl <100993536+RLMNLDEV@users.noreply.github.com>
2023-01-12 17:00:29 +0800

    Update streamlit_app.py
    add input for adding fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,9 @@ streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 # my_data_row = my_cur.fetchone()
 # streamlit.text("Hello from Snowflake:")
